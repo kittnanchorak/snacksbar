338d036c831f71c7fde92432ed3ad35227523fb9
kittnanchorak <jabbskyblue@gmail.com>
2024-09-14 19:25:07 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'snacksbar',
+  webDir: 'www'
+};
+
+export default config;
